4e9893373d7080446a9455873c8ac457f506b2f2
Leandro Madeira <leandromad140@hotmail.com>
2021-07-29 10:57:40 -0400

    caixa eletronico salvo

---
+++ Caixa eletronico/caixa eletronico.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+
+
+void flush_in()
+{
+   int ch;
+
+   while( (ch = fgetc(stdin)) != EOF && ch != '\n' ){}
+}
+
+int main(int argc, char const *argv[])
+{
+    int nota_500, nota_150, nota_50, nota_20, nota_10, nota_5, nota_2, nota_1;
+    int opc;
+
+    nota_500 = 100;
+    nota_150 = 200; 
+    nota_50 = 400;
+    nota_20 = 800;
+    nota_10 = 1600;
+    nota_5 = 3200;
+    nota_2 = 6400;
+    nota_1 = 12800;
+    
+    total = (500*nota_500) + (150*nota_150) + (50*nota_50) + (20*nota_20) + (10*nota_10) + (5*nota_5) + (2*nota_2) + (1*nota_1);
+
+    do{
+        
+    }while
+    printf("---Bem vindo ao caixa eletronico!--");
+        printf("1-Saque");
+        printf("2-gerente");
+        printf("3-finalizar");
+        scanf("%d", &)
+    switch (opc)
+    {
+    case 1
+
+        break;
+    
+    default:
+        break;
+    }{
+        
+    return 0;
+}
