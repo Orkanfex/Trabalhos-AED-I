c7983e62d222e9f8ad83e94fd32de3df0e2c1023
Leandro Madeira <leandromad140@hotmail.com>
2021-07-29 11:15:50 -0400

    inicio menu caixa eletrônico

--- Caixa eletronico/caixa eletronico.c
+++ Caixa eletronico/caixa eletronico.c
@@ -11,7 +11,8 @@ void flush_in()
 int main(int argc, char const *argv[])
 {
     int nota_500, nota_150, nota_50, nota_20, nota_10, nota_5, nota_2, nota_1;
-    int opc;
+    int opc, cpf;
+    float valor;
 
     nota_500 = 100;
     nota_150 = 200; 
@@ -25,22 +26,47 @@ int main(int argc, char const *argv[])
     total = (500*nota_500) + (150*nota_150) + (50*nota_50) + (20*nota_20) + (10*nota_10) + (5*nota_5) + (2*nota_2) + (1*nota_1);
 
     do{
-        
-    }while
-    printf("---Bem vindo ao caixa eletronico!--");
-        printf("1-Saque");
-        printf("2-gerente");
-        printf("3-finalizar");
-        scanf("%d", &)
+        printf("---Bem vindo ao caixa eletronico!--\n");
+        printf("1-Saque\n");
+        printf("2-gerente\n");
+        printf("3-finalizar\n");
+        scanf("%d", &opc);
+        flush_in();
+
+        if(opc<1 || opc>3){
+            printf("Opcao inxistente, digite uma opção válida\n")
+        }
+
+    }while(opc!=1 || opc!=2 || opc!=3)
+    
     switch (opc)
     {
-    case 1
+    case 1:
+        printf("CPF: ");
+        scanf("%d", &cpf);
+        //VALIDAÇÃO DO CPF
+
+
+
+
+
+        //será executado se o CPF for válido
+        printf("Insira o valor desejado: ");
+        scanf("%f", &valor);    
+        flush_in();
+
+        
+        break;
+    case 2:
 
         break;
-    
+    case 3:
+
+        break;
+
     default:
         break;
-    }{
+    }
         
     return 0;
 }
